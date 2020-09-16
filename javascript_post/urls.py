6726df2aa329807039637d1197f91f8eb68a2aef
victor-s-santos <victor.s.silva@unesp.br>
2020-09-16 09:57:35 -0300

    login and register system has been created

--- javascript_post/urls.py
+++ javascript_post/urls.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from register import views as register_v
 from core import views as core_v
 
 urlpatterns = [
+    #first page
     path('', core_v.index, name='home'),
+    #register
+    path('register/', register_v.register, name='register'),
+    #login
+    path('', include('django.contrib.auth.urls')),
+    #admin
     path('admin/', admin.site.urls),
 ]
--- register/views.py
+++ register/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from django.contrib import messages
+#from django.contrib.auth.forms import UserCreationForm
+from .forms import RegisterForm
 
 # Create your views here.
+def register(request):
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            messages.success(request, 'User has been registered successfully')
+            form.save()
+            return redirect('home')
+    else:
+        form = RegisterForm()
+    return render(request, 'register/register.html', {"form": form})
